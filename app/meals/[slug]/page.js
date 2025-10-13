7d7e61903631ddf29d758a0ef2ba83acca264229
Mahmoud Mostafa <156802364+M7moudMostafa@users.noreply.github.com>
2025-10-13 13:29:56 +0300

    Add Meal Details Page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
-const mealDetailsPage = ({ params }) => {
+const mealDetailsPage = async ({ params }) => {
+  const meal = await getMeal(params.slug);
+  if(!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+
   return (
-    <h1>meal - {params.slug}</h1>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{
+          __html: meal.instructions,
+        }}></p>
+      </main>
+    </>
   )
 }
 
